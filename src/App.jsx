b2bbf15ac4eb038e3cd6983567c1568c9431d20b
heriantonius <heriantonius459951@gmail.com>
2020-10-21 12:14:10 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from './pages/LoginPage';
+import Register from './pages/RegisterPage';
+import Homepage from './pages/Homepage';
+import "./App.css";
+import Cart from './pages/Cart';
+import User from './pages/DashboardPage';
+import Navbar from './components/navbar/Navbar';
+function App() {
+  return (
+    <Router>
+      <Switch>
+		<Route exact path="/">
+          <Navbar />
+          <Homepage />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+		<Route path="/cart">
+          <Cart />
+        </Route>
+		<Route path="/dashboard">
+			<User />  
+		</Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,38 +1,4 @@
 import React from 'react';
-<<<<<<< Updated upstream
-import { Navbar, Form, Button } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from "../../assets/img/logo.svg";
-import search from "../../assets/img/search.png";
-import styles from './Navbar.module.css';
-
-
-export default function Nav() {
-    return (
-        <Navbar  className={styles.Nav}>
-        <Navbar.Brand href="#home">
-          <img src={logo}
-           height="38"
-           width="81"/>
-            </Navbar.Brand>
-                <div className={styles.Searchlogo}>
-                <img src={search} 
-                height="20"
-                width="20"/> 
-                </div>
-                <Form.Control className={styles.Form} type="text" placeholder="Search Product ..." />
-                <Button className={styles.Button}>Sign Up</Button>
-                <a className={styles.Login} href="">Log In</a>
-              
-            </Navbar>
-            
-                
-    )
-}
-
-
-
-=======
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
 import logo from '../../assets/img/logo.svg';
@@ -61,4 +27,3 @@ export default function Navbar() {
        
     )
 }
->>>>>>> Stashed changes
