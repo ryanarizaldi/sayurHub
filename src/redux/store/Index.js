45129187067b641bd126f8125da8f94e9f9d519a
ryan <ryanarizaldi@gmail.com>
2020-11-17 21:53:05 +0800

    ini bukan anggi

--- src/redux/store/Index.js
+++ src/redux/store/Index.js
@@ -1,21 +1,26 @@
-import { createStore, compose, applyMiddleware, combineReducers  } from "redux";
+import { createStore, compose, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import reducer from "../reducer/Reducer";
 import reducerCart from "../reducer/ReducerCart";
 import reducerDiscussion from "../reducer/ReducerDiscussion";
 
-
 const rootReducer = combineReducers({
-	index: reducer,
-	cart: reducerCart,
-	discussion: reducerDiscussion
-})
+  index: reducer,
+  cart: reducerCart,
+  discussion: reducerDiscussion,
+});
 
-const allEnhancers = compose(
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// const allEnhancers = compose(
+//   applyMiddleware(thunk),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  /* preloadedState, */ composeEnhancers(applyMiddleware(thunk))
 );
 
-const store = createStore(rootReducer, allEnhancers);
+// const store = createStore(rootReducer, allEnhancers);
 
 export default store;
